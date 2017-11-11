f15506c430715fd857e5c0b48eb0dd5b47b6c56f
Justin Firth <jmfirth@gmail.com>
2017-11-11 00:36:05 -0500

    Remove most of the extraneous modules and examples; Remove an import causing a compile issue.  Bump version

--- TestApp/update-npm-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Updating our npm packages...'
-rm -rf node_modules/mobile-center-link-scripts
-rm -rf node_modules/mobile-center
-rm -rf node_modules/mobile-center-crashes
-rm -rf node_modules/mobile-center-analytics
-rm -rf node_modules/mobile-center-push
-npm install
--- TestApp34/ios/Pods/Target Support Files/Pods-TestApp34/Pods-TestApp34-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestApp34 : NSObject
-@end
-@implementation PodsDummy_Pods_TestApp34
-@end
